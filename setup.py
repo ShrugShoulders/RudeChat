6a74296f6fd1265fb9fa90c972fbd2eeba10b07f
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2023-12-29 22:30:28 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RudeChat",
+    version="3.0.0",
+    description="RudeChat is a Python IRC client designed to be fast, portable, and fun.",
+    author="Irish",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    scripts=["src/rudechat3/main.py"],
+    install_requires=[
+        'pytz',
+        'asyncio',
+        'irctokens',
+        'plyer',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        "rudechat3": [
+            "Art/*",
+            "Sounds/*",
+            "Fortune Lists/*",
+            "*.rude",
+            "nickname_colors.json",
+            "rude.ico",
+            "rude.png",
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'rudechat=rudechat3.main:main',
+        ],
+    },
+)
