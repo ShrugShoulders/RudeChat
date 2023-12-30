72b32f6fb97e48b109cc6a8852ac3e7023fa0155
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2023-12-30 02:13:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
             "Art/*",
             "Sounds/*",
             "Fortune Lists/*",
+            "Splash/*",
             "*.rude",
             "nickname_colors.json",
             "rude.ico",
