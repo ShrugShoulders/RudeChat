d16ca8b993f2e7b5caaa2929ce2e1f5e1a965086
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2024-03-21 10:01:33 -0600

    Update linux_install.sh
    Updated install script.

--- linux_install.sh
+++ linux_install.sh
@@ -1,8 +1,8 @@
 echo "Installing RudeChat3..."
-pip install pytz
-pip install asyncio
-pip install irctokens
-pip install aiofiles
-pip install plyer
-pip install colorchooser
+pip install pytz --break-system-packages
+pip install asyncio --break-system-packages
+pip install irctokens --break-system-packages
+pip install aiofiles --break-system-packages
+pip install plyer --break-system-packages
+pip install tkcolorpicker --break-system-packages
 pip install .
