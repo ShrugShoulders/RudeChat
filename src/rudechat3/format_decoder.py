4c148de97dd808e1c28f7d543802bdc7373af16f
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2024-03-23 14:38:49 -0600

    Update format_decoder.py
    add support for inverse control characters and streamlined the color code catching.

--- src/rudechat3/format_decoder.py
+++ src/rudechat3/format_decoder.py
@@ -7,6 +7,7 @@ class Attribute:
     italic: bool = False
     underline: bool = False
     strikethrough: bool = False
+    inverse: bool = False 
     colour: int = 0
     background: int = 0
 
@@ -31,25 +32,44 @@ def decoder(input_text: str) -> List[Tuple[str, List[Attribute]]]:
             case '\x1E':
                 if not any(attr.strikethrough for attr in current_attributes):
                     current_attributes.append(Attribute(strikethrough=True))
+            case '\x16':
+                existing_colours = [attr for attr in current_attributes if attr.colour or attr.background]
+                if existing_colours:
+                    # Assuming there is at most one colour and background attribute at any time
+                    current_colour = existing_colours[0].colour
+                    current_background = existing_colours[0].background
+                    # Clear existing color and background
+                    current_attributes = [attr for attr in current_attributes if not (attr.colour or attr.background)]
+                    # Swap the colors
+                    current_attributes.append(Attribute(colour=current_background, background=current_colour))
+                else:
+                    # If no colors were set, default to swapping black (88) and white (1) or appropriate values
+                    current_attributes.append(Attribute(colour=88, background=1))
             case '\x03':
                 current_attributes = []
                 colour_code = ''
+                background_code = ''
+                
+                # Extracting color code
                 while c_index + 1 < len(input_text) and input_text[c_index + 1].isdigit():
                     c_index += 1
                     colour_code += input_text[c_index]
-                if colour_code:
-                    background_num = 0
-                    if c_index + 1 < len(input_text) and input_text[c_index + 1] == ',':
+                
+                # Extracting background code
+                if c_index + 1 < len(input_text) and input_text[c_index + 1] == ',':
+                    c_index += 1
+                    while c_index + 1 < len(input_text) and input_text[c_index + 1].isdigit():
                         c_index += 1
-                        background_code = ''
-                        while c_index + 1 < len(input_text) and input_text[c_index + 1].isdigit():
-                            c_index += 1
-                            background_code += input_text[c_index]
-                        try:
-                            background_num = int(background_code)
-                        except ValueError:
-                            background_num = 0
-                    new_attribute = Attribute(colour=int(colour_code), background=background_num)
+                        background_code += input_text[c_index]
+                
+                # Converting codes to integers
+                try:
+                    colour = int(colour_code)
+                    background = int(background_code) if background_code else 0
+                except ValueError:
+                    pass
+                else:
+                    new_attribute = Attribute(colour=colour, background=background)
                     if new_attribute not in current_attributes:
                         current_attributes.append(new_attribute)
             case '\x0F':
@@ -68,4 +88,3 @@ def decoder(input_text: str) -> List[Tuple[str, List[Attribute]]]:
         c_index += 1
 
     return output
-    #thing
