b27f4731433a6931311da08ce1902ef0bf07487a
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2024-04-13 10:11:30 -0600

    Update main.py
    Update to main.py
    moved initialize_clients to it's own file.

--- src/rudechat3/main.py
+++ src/rudechat3/main.py
@@ -20,54 +20,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from .rude_gui import RudeGui
 # Import Client
 from .rude_client import RudeChatClient
-# Import Channel List Window.
-from .list_window import ChannelListWindow
+# Import Initializer
+from .init_clients import initialize_clients
 # Everything else.
 from .shared_imports import *
-
-MAX_RETRIES = 5  # Max number of times to retry on semaphore error
-RETRY_DELAY = 5  # Time in seconds to wait before retrying
-
-async def initialize_clients(app):
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-
-    # Construct absolute paths for conf.*.rude files
-    config_files = [os.path.join(script_directory, f) for f in os.listdir(script_directory) if f.startswith("conf.") and f.endswith(".rude")]
-    config_files.sort()
-
-    if not config_files:
-        print("No .rude configuration files found.")
-        return
-
-    for i, config_file in enumerate(config_files):
-        try:
-            await app.init_client_with_config(config_file, f'Server_{i+1}')
-        except OSError as e:
-            if e.winerror == 121:  # The semaphore timeout error
-                retries = 0
-                while retries < MAX_RETRIES:
-                    retries += 1
-                    print(f"Semaphore timeout error. Retrying {retries}/{MAX_RETRIES}...")
-                    await asyncio.sleep(RETRY_DELAY)
-                    try:
-                        await app.init_client_with_config(config_file, f'Server_{i+1}')
-                        break
-                    except OSError as e:
-                        if e.winerror != 121:
-                            print(f"An unexpected error occurred: {str(e)}")
-                            break
-                else:
-                    print("Max retries reached. Skipping this server.")
-            else:
-                print(f"An unexpected error occurred: {str(e)}")
-        except Exception as e:
-            print(f"Failed to connect to Server_{i+1} due to {e}. Proceeding to the next server.")
-
-    # Update the Listbox with the new list of servers
-    if app.server_listbox.size() > 0:
-        first_server = app.server_listbox.get(0)
-        app.server_var.set(first_server)
-        app.on_server_change(None)
 
 def main():
     root = tk.Tk()
