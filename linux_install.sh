db012fe392ff69c72e75182bc3f8d384f9ca5907
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2024-04-25 00:29:23 -0600

    Update linux_install.sh

--- linux_install.sh
+++ linux_install.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+
+if [ "$(uname -s)" = "Linux" ] && [ -f "/etc/debian_version" ]; then
+    echo "Debian detected. Using pip with --break-system-packages option."
+    pip_install="pip install --break-system-packages"
+else
+    echo "Non-Debian system detected. Using pip without --break-system-packages option."
+    pip_install="pip install"
+fi
+
 echo "Installing RudeChat3..."
-pip install pytz --break-system-packages
-pip install asyncio --break-system-packages
-pip install irctokens --break-system-packages
-pip install aiofiles --break-system-packages
-pip install plyer --break-system-packages
-pip install tkcolorpicker --break-system-packages
-pip install colorchooser --break-system-packages
-pip install .
+$pip_install pytz
+$pip_install asyncio
+$pip_install irctokens
+$pip_install aiofiles
+$pip_install plyer
+$pip_install tkcolorpicker
+$pip_install colorchooser
+$pip_install .
+
+echo "Installation completed."
