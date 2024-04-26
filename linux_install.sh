68818f1a0f3d96c3ddc36c9def00cf1a5ec7a102
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2024-04-25 21:17:50 -0400

    Update linux_install.sh
    Updated script to find debian, use apt-get, and if not use pip.

--- linux_install.sh
+++ linux_install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ "$(uname -s)" = "Linux" ] && [ -f "/etc/debian_version" ]; then
-    echo "Debian detected. Using pip with --break-system-packages option."
-    pip_install="pip install --break-system-packages"
-else
-    echo "Non-Debian system detected. Using pip without --break-system-packages option."
-    pip_install="pip install"
-fi
+install_dependencies() {
+    # Check if apt-get is available
+    if command -v apt-get &>/dev/null; then
+        echo "Debian/Ubuntu detected. Installing Python dependencies using apt-get..."
+        sudo apt-get update
+        sudo apt-get install -y python3-pytz python3-asyncio python3-irctokens python3-aiofiles python3-plyer python3-tk python3-colorchooser
+    else
+        echo "Non-Debian system detected. Installing Python dependencies using pip..."
+        pip install pytz asyncio irctokens aiofiles plyer tkcolorpicker colorchooser
+    fi
+}
+
+# Install Python dependencies
+install_dependencies
 
 echo "Installing RudeChat3..."
-$pip_install pytz
-$pip_install asyncio
-$pip_install irctokens
-$pip_install aiofiles
-$pip_install plyer
-$pip_install tkcolorpicker
-$pip_install colorchooser
-$pip_install .
+pip install .
 
 echo "Installation completed."
