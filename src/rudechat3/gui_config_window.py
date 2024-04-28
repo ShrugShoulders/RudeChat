25e8413d3066ef822b61c71f00c9b83b326099a4
Connor Kim <connorkim.kim3@gmail.com>
2024-04-28 17:12:03 -0600

    RudeChat3 Updates

--- src/rudechat3/gui_config_window.py
+++ src/rudechat3/gui_config_window.py
@@ -17,6 +17,8 @@ class GuiConfigWindow:
 
         # Create labels and entry fields for main GUI settings
         tk.Label(self.root, text="Main GUI Settings").pack()
+        self.create_setting_entry("Your Nickname Color:", config.get('GUI', 'main_nickname_color'))
+        self.create_setting_entry("Generage Nickname Colors:", config.get('GUI', 'generate_nickname_colors'))
         self.create_setting_entry("Master:", config.get('GUI', 'master_color'))
         self.create_setting_entry("Font Family:", config.get('GUI', 'family'))
         self.create_setting_entry("Font Size:", config.get('GUI', 'size'))
@@ -29,15 +31,24 @@ class GuiConfigWindow:
         tk.Label(self.root, text="Widget Settings").pack()
         self.create_setting_entry("User List Foreground:", config.get('WIDGETS', 'users_fg'))
         self.create_setting_entry("User List Background:", config.get('WIDGETS', 'users_bg'))
+        self.create_setting_entry("User Label Foreground:", config.get('WIDGETS', 'user_label_fg'))
+        self.create_setting_entry("User Label Background:", config.get('WIDGETS', 'user_label_bg'))
         self.create_setting_entry("Channel List Foreground:", config.get('WIDGETS', 'channels_fg'))
         self.create_setting_entry("Channel List Background:", config.get('WIDGETS', 'channels_bg'))
+        self.create_setting_entry("Channel Label Foreground:", config.get('WIDGETS', 'channel_label_fg'))
+        self.create_setting_entry("Channel Label Background:", config.get('WIDGETS', 'channel_label_bg'))
         self.create_setting_entry("Input Foreground:", config.get('WIDGETS', 'entry_fg'))
         self.create_setting_entry("Input Insert Background:", config.get('WIDGETS', 'entry_insertbackground'))
         self.create_setting_entry("Input Background:", config.get('WIDGETS', 'entry_bg'))
-        self.create_setting_entry("Input Label Foreground:", config.get('WIDGETS', 'entry_label_bg'))
-        self.create_setting_entry("Input Label Background:", config.get('WIDGETS', 'entry_label_fg'))
-        self.create_setting_entry("Server List Background:", config.get('WIDGETS', 'server_listbox_bg'))
+        self.create_setting_entry("Input Label Foreground:", config.get('WIDGETS', 'entry_label_fg'))
+        self.create_setting_entry("Input Label Background:", config.get('WIDGETS', 'entry_label_bg'))
         self.create_setting_entry("Server List Foreground:", config.get('WIDGETS', 'server_listbox_fg'))
+        self.create_setting_entry("Server List Background:", config.get('WIDGETS', 'server_listbox_bg'))
+        self.create_setting_entry("Server Label Foreground:", config.get('WIDGETS', 'servers_label_fg'))
+        self.create_setting_entry("Server Label Background:", config.get('WIDGETS', 'servers_label_bg'))
+        self.create_setting_entry("Topic Label Foreground:", config.get('WIDGETS', 'topic_label_fg'))
+        self.create_setting_entry("Topic Label Background:", config.get('WIDGETS', 'topic_label_bg'))
+        self.create_setting_entry("Tab Completion:", config.get('WIDGETS', 'tab_complete_terminator'))
 
         # Button to save changes
         ttk.Button(self.root, text="Save", command=self.save_changes).pack()
@@ -45,14 +56,19 @@ class GuiConfigWindow:
 
     def create_setting_entry(self, setting_name, default_value):
         frame = tk.Frame(self.root)
-        frame.pack(fill="x", padx=10, pady=5)
+        frame.pack(fill="x")
 
-        label = tk.Label(frame, text=setting_name, width=20, anchor="w")
-        label.grid(row=0, column=0, padx=(0, 10))
+        label = tk.Label(frame, text=setting_name, anchor="w")
+        label.pack(side="left")
+
+        # Calculate the appropriate width for the label based on the length of setting_name
+        label_width = max(10, len(setting_name))  # Minimum width of 10 pixels
+        label.config(width=label_width)
 
         entry = tk.Entry(frame)
         entry.insert(0, default_value)
-        entry.grid(row=0, column=1, sticky='w') 
+        entry.pack(side="right", fill="x", expand=True)
+
         setattr(self, setting_name, entry)
 
     def save_changes(self):
@@ -60,6 +76,8 @@ class GuiConfigWindow:
         config.read(self.config_file)
 
         # Update config with new values
+        config.set('GUI', 'main_nickname_color', getattr(self, "Your Nickname Color:").get())
+        config.set('GUI', 'generate_nickname_colors', getattr(self, "Generage Nickname Colors:").get())
         config.set('GUI', 'master_color', getattr(self, "Master:").get())
         config.set('GUI', 'family', getattr(self, "Font Family:").get())
         config.set('GUI', 'size', getattr(self, "Font Size:").get())
@@ -70,15 +88,24 @@ class GuiConfigWindow:
 
         config.set('WIDGETS', 'users_fg', getattr(self, "User List Foreground:").get())
         config.set('WIDGETS', 'users_bg', getattr(self, "User List Background:").get())
+        config.set('WIDGETS', 'user_label_bg', getattr(self, "User Label Background:").get())
+        config.set('WIDGETS', 'user_label_fg', getattr(self, "User Label Foreground:").get())
         config.set('WIDGETS', 'channels_fg', getattr(self, "Channel List Foreground:").get())
         config.set('WIDGETS', 'channels_bg', getattr(self, "Channel List Background:").get())
+        config.set('WIDGETS', 'channel_label_fg', getattr(self, "Channel Label Foreground:").get())
+        config.set('WIDGETS', 'channel_label_bg', getattr(self, "Channel Label Background:").get())
         config.set('WIDGETS', 'entry_fg', getattr(self, "Input Foreground:").get())
         config.set('WIDGETS', 'entry_insertbackground', getattr(self, "Input Insert Background:").get())
         config.set('WIDGETS', 'entry_bg', getattr(self, "Input Background:").get())
-        config.set('WIDGETS', 'entry_label_bg', getattr(self, "Input Label Foreground:").get())
-        config.set('WIDGETS', 'entry_label_fg', getattr(self, "Input Label Background:").get())
+        config.set('WIDGETS', 'entry_label_bg', getattr(self, "Input Label Background:").get())
+        config.set('WIDGETS', 'entry_label_fg', getattr(self, "Input Label Foreground:").get())
         config.set('WIDGETS', 'server_listbox_bg', getattr(self, "Server List Background:").get())
         config.set('WIDGETS', 'server_listbox_fg', getattr(self, "Server List Foreground:").get())
+        config.set('WIDGETS', 'servers_label_bg', getattr(self, "Server Label Background:").get())
+        config.set('WIDGETS', 'servers_label_fg', getattr(self, "Server Label Foreground:").get())
+        config.set('WIDGETS', 'topic_label_bg', getattr(self, "Topic Label Background:").get())
+        config.set('WIDGETS', 'topic_label_fg', getattr(self, "Topic Label Foreground:").get())
+        config.set('WIDGETS', 'tab_complete_terminator', getattr(self, "Tab Completion:").get())
 
         # Write the updated config back to the file
         with open(self.config_file, 'w') as configfile:
