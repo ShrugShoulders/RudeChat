0a89529ddc119461f51641d690f2ee9533e6a050
Connor Kim <connorkim.kim3@gmail.com>
2024-06-26 00:39:42 -0600

    Big change in the way znc is now sync'd, it build a list of PRIVMSG tokens and processes that list after the first time out happens, no matter which time out that is. It also gives user feedback for the whole process

--- src/rudechat3/rude_client.py
+++ src/rudechat3/rude_client.py
@@ -323,8 +323,8 @@ class RudeChatClient:
         count_366 = 0
         got_topic = 0
         last_366_time = None
-        TIMEOUT_SECONDS = 0.2
-        MAX_WAIT_TIME = 60
+        TIMEOUT_SECONDS = 0.16
+        MAX_WAIT_TIME = 80
         PRIVMSGTOKENS = []
 
         start_time = asyncio.get_event_loop().time()
@@ -332,14 +332,14 @@ class RudeChatClient:
         def reset_timer(symbol):
             nonlocal last_366_time
             nonlocal sync
-            if motd_received:
-                if sync:
-                    self.gui.insert_text_widget(f'Syncing with ZNC: ')
-                    sync = False
-                else:
-                    self.gui.insert_text_widget(f'\x0303\x02{symbol}\x0F')
             if not self.use_auto_join:
-                last_366_time = asyncio.get_event_loop().time()
+                last_366_time = asyncio.get_event_loop().time() + 0.01
+                if motd_received:
+                    if sync:
+                        self.gui.insert_text_widget(f'Syncing with ZNC: ')
+                        sync = False
+                    else:
+                        self.gui.insert_text_widget(f'\x0303\x02{symbol}\x0F')
 
         def check_timeout():
             nonlocal last_366_time
@@ -389,7 +389,6 @@ class RudeChatClient:
                         self.gui.insert_text_widget(f'Connected to the server: {self.server}:{self.port}\n')
                         received_001 = True
                         self.gui.insert_and_scroll()
-
                     case "002":
                         self.server_message_handler(tokens)
                     case "003":
@@ -420,13 +419,16 @@ class RudeChatClient:
                     case "NICK":
                         await self.handle_nick(tokens)
                     case "JOIN":
-                        await self.join_znc_channel(tokens)
-                        if not self.use_auto_join:
+                        if self.znc_connection:
+                            await self.join_znc_channel(tokens)
                             reset_timer("#")
                     case "PRIVMSG":
-                        PRIVMSGTOKENS.append(tokens)
-                        if not self.use_auto_join:
+                        if self.znc_connection:
+                            PRIVMSGTOKENS.append(tokens)
                             reset_timer("&")
+                        else:
+                            await self.handle_privmsg(tokens)
+
                     case "MODE":
                         self.handle_mode(tokens)
                     case "305":
@@ -482,8 +484,10 @@ class RudeChatClient:
                             if self.use_auto_join:
                                 await self.automatic_join()
                                 znc_connected = True
+                                reset_timer("!")
                             elif not self.use_auto_join:
                                 znc_connected = True
+                                reset_timer("!")
                         elif sasl_authenticated and self.isupport_flag and not self.znc_connection:
                             if self.use_auto_join:
                                 await self.automatic_join()
@@ -516,17 +520,28 @@ class RudeChatClient:
                         self.gui.insert_and_scroll()
                 if check_timeout():
                     # Timeout occurred
-                    for tokens in PRIVMSGTOKENS:
-                        await self.handle_privmsg(tokens)
-                    return
+                    if self.znc_connection:
+                        symbol_list = ['!', '@', '#', '$', '%', '&']
+                        self.gui.insert_text_widget(f'\n\x0303\x02Processing Messages: \x0F')
+                        for tokens in PRIVMSGTOKENS:
+                            self.gui.insert_text_widget(f'\x0303\x02{random.choice(symbol_list)}\x0F')
+                            await self.handle_privmsg(tokens)
+                        return
 
             # Check for overall timeout
             elapsed_time = asyncio.get_event_loop().time() - start_time
             if elapsed_time > MAX_WAIT_TIME:
-                self.gui.insert_text_widget("\nMaximum sync time exceeded\n")
-                for tokens in PRIVMSGTOKENS:
-                    await self.handle_privmsg(tokens)
-                return
+                if self.znc_connection:
+                    symbol_list = ['!', '@', '#', '$', '%', '&']
+                    self.gui.insert_text_widget("\nMaximum sync time exceeded\n")
+                    self.gui.insert_text_widget(f'\x0303\x02Processing Messages: \x0F')
+                    for tokens in PRIVMSGTOKENS:
+                        self.gui.insert_text_widget(f'\x0303\x02{random.choice(symbol_list)}\x0F')
+                        await self.handle_privmsg(tokens)
+                    return
+                else:
+                    self.gui.insert_text_widget("\nMaximum sync time exceeded\n")
+                    return
 
     async def handle_cap(self, tokens):
         if not self.sasl_enabled:
