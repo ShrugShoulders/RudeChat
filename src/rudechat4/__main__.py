ed87775c1ab829dd6afb8beb6ff8060b7520a17c
Bash Elliott <bashelliott@gmail.com>
2025-06-01 19:41:54 +1000

    version change

--- src/rudechat4/__main__.py
+++ src/rudechat4/__main__.py
@@ -109,7 +109,7 @@ def __main__():
     app = QApplication(sys.argv)
     app.setApplicationName("RudeChat4")
     app.setApplicationDisplayName("RudeChat4")
-    app.setApplicationVersion("4.1.0")
+    app.setApplicationVersion("4.1.1")
     app.setWindowIcon(QIcon(ICON_FILE))
     app.setDesktopFileName("rudechat4")
     first_run = FirstRun(app)
