5a2d985d68410da71133f8c5d75e48e38e33c9ac
Bash Elliott <bashelliott@gmail.com>
2025-06-30 12:49:41 +1000

    ver change

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PKGS = [
     'rudechat4.Util'
 ]
 
-VERSION = '4.1.1'
+VERSION = '4.1.2'
 APP = ['src/rudechat4/__main__.py']
 OPTIONS = {
     'iconfile': 'src/rudechat4/Resources/Icons/rude.icns', 
