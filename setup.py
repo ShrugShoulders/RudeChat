91545e6bbd0675f4837ee283906713d542df1a45
ShrugShoulders <connorkim.kim3@outlook.com>
2025-06-30 19:16:17 -0600

    stupid setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages, Extension
+from Cython.Build import cythonize
 import glob
 import platform
 
-PKGS = [
-    'rudechat4',
-    'rudechat4.Client',
-    'rudechat4.Components',
-    'rudechat4.GUI',
-    'rudechat4.Util',
-    'rudechat4.Cython'
-]
-cython_so_files = glob.glob('src/rudechat4/Cython/*.so')
-
 VERSION = '4.1.2'
+
+def isMac():
+    return platform.system() == 'Darwin'
+
+# Build the Cython extension
+ext_modules = cythonize([
+    Extension(
+        "rudechat4.Cython.decoder_cython",
+        sources=["src/rudechat4/Cython/decoder_cython.pyx"],
+    )
+])
+
 APP = ['src/rudechat4/__main__.py']
 OPTIONS = {
     'iconfile': 'src/rudechat4/Resources/Icons/rude.icns', 
@@ -28,14 +31,12 @@ OPTIONS = {
     }
 }
 
-def isMac():
-    return platform.system() == 'Darwin'
-
 setup(
-    packages = find_namespace_packages(where='src'),
+    name='RudeChat',
+    version=VERSION,
+    packages=find_namespace_packages(where='src'),
     package_dir={"": "src"},
     include_package_data=True,
-    scripts=["src/rudechat4/__main__.py"],
     install_requires=[
         'pytz',
         'asyncio',
@@ -46,20 +47,24 @@ setup(
         'emoji',
         'PyQt6',
         'requests',
+        'charset_normalizer',  # Add to fix RequestsDependencyWarning
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    package_data ={"rudechat4": ["Cython/*"]},
+    package_data={
+        "rudechat4": ["Cython/*"],  # include .pyx and other static files
+    },
     entry_points={
         'console_scripts': [
             'rudechat=rudechat4.__main__:main',
         ],
     },
-    app = ['src/rudechat4/__main__.py'] if isMac() else [],
-    data_files = [('rudechat4/Cython', cython_so_files),],
-    options = {'py2app': OPTIONS} if isMac() else {},
-    setup_requires = ['py2app'] if isMac() else []
+    app=APP if isMac() else [],
+    data_files=[],
+    ext_modules=ext_modules,
+    options={'py2app': OPTIONS} if isMac() else {},
+    setup_requires=['py2app'] if isMac() else [],
 )
