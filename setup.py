f2a88cf118d8cc87319008bf153c29d01bf1aaee
Connor Kim <connorkim.kim3@outlook.com>
2025-11-14 08:53:34 -0700

    version change

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PKGS = [
     'rudechat4.Util'
 ]
 
-VERSION = '4.1.2'
+VERSION = '4.2.0'
 APP = ['src/rudechat4/__main__.py']
 OPTIONS = {
     'iconfile': 'src/rudechat4/Resources/Icons/rude.icns', 
